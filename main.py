6ae607bd1037e701a91665adce909ddd6a2e1fb8
ghetto23 <92057072+ghetto23@users.noreply.github.com>
2022-01-17 23:02:15 +0300

    Add files via upload
    first upload

---
+++ main.py
@@ -0,0 +1,53 @@
+import requests
+
+def boom():
+    print('''
+ 
+                       $$$$$$$$$$$$$$$$$$$$$$$$$
+                       $$$$$$$$$$$$$$$$$$$$$$$$$
+                       $$$$$'`$$$$$$$$$$$$$'`$$$
+                       $$$$$$  $$$$$$$$$$$  $$$$
+                       $$$$$$$  '$/ `/ `$' .$$$$
+                       $$$$$$$$. i  i  /! .$$$$$
+                       $$$$$$$$$.--'--'   $$$$$$
+                       $$^^$$$$$'        J$$$$$$
+                       $$$   ~""   `.   .$$$$$$$
+                       $$$$$e,      ;  .$$$$$$$$
+                       $$$$$$$$$$$.'   $$$$$$$$$
+                       $$$$$$$$$$$$.    $$$$$$$$
+                       $$$$$$$$$$$$$     $$$$$$$
+                       -------------------------
+                           Admin Panel Finder
+                           Coded by ghetto23
+                          github.com/ghetto23
+                       -------------------------
+ ''')
+
+
+
+
+
+def looky():
+    ac = open('admin.txt','r')
+    print("\n\t    Domain giriniz. Not! http ve ya https yazmayin\n")
+    link = input("> ")
+    print("\nBasliyoruzz ..\n")
+
+    while True:
+        links = ac.readline()
+        deneme = f'http://{link}/{links}'
+        try:
+            loky = requests.get(deneme)
+            if loky.status_code == 200 or loky.status_code == 305:
+                print(f"Giris paneli: {deneme}")
+                break
+        except requests.exceptions.ConnectionError:
+            continue
+
+if __name__ == '__main__':
+    boom()
+    looky() 
+
+
+
+        
