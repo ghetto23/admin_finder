cf8d43b49210ebb61019e339b69f65569f1cff58
ghetto23 <92057072+ghetto23@users.noreply.github.com>
2022-01-19 00:37:11 +0400

    func chngd

--- main.py
+++ main.py
@@ -1,5 +1,26 @@
 import requests
 
+
+def looky():
+    ac = open('admin.txt','r')
+    print("\n\t    Domain giriniz. Not! http ve ya https yazmayin\n")
+    link = input("> ")
+    print("\nBasliyoruzz ..\n")
+
+    while True:
+        links = ac.readline()
+        deneme = f'http://{link}/{links}'
+        try:
+            loky = requests.get(deneme)
+            if loky.status_code == 200 or loky.status_code == 305:
+                print(f"Giris paneli: {deneme}")
+                break
+        except requests.exceptions.ConnectionError:
+            continue
+
+
+
+
 def boom():
     print('''
  
@@ -23,26 +44,6 @@ def boom():
                        -------------------------
  ''')
 
-
-
-
-
-def looky():
-    ac = open('admin.txt','r')
-    print("\n\t    Domain giriniz. Not! http ve ya https yazmayin\n")
-    link = input("> ")
-    print("\nBasliyoruzz ..\n")
-
-    while True:
-        links = ac.readline()
-        deneme = f'http://{link}/{links}'
-        try:
-            loky = requests.get(deneme)
-            if loky.status_code == 200 or loky.status_code == 305:
-                print(f"Giris paneli: {deneme}")
-                break
-        except requests.exceptions.ConnectionError:
-            continue
 
 if __name__ == '__main__':
     boom()
